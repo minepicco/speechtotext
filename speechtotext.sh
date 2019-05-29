160965b6b9882a3a8258954b451b6cf30663927e
Minehiko Nohara <minepicco@gmail.com>
2019-05-29 20:07:34 +0900

    Update speechtotext.sh

--- speechtotext.sh
+++ speechtotext.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 
+curl -s -H "Content-Type: application/json"     -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) "https://www.googleapis.com/storage/v1/b/$bucket/o"
+
+curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$(gcloud auth application-default print-access-token)  https://speech.googleapis.com/v1/speech:longrunningrecognize -d @request.json
