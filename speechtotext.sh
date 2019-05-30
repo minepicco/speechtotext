2f2ae72e2fabfd1deffc4d34e984f6a8fb80e8dd
Minehiko Nohara <minepicco@gmail.com>
2019-05-30 12:48:31 +0900

    Update speechtotext.sh

--- speechtotext.sh
+++ speechtotext.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-.config
+bucket="<YOUR BUCKET NAME>"
 export GOOGLE_APPLICATION_CREDENTIALS="sa.json"
 
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) "https://www.googleapis.com/storage/v1/b/$bucket/o" | jq -r ".items[] | .name" > objects.out
 
 while read line
 do
-    oname=$line
+    name=$line
     jobid=$(curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$(gcloud auth application-default print-access-token)  https://speech.googleapis.com/v1/speech:longrunningrecognize -d '{"config": {"encoding":"ENCODING_UNSPECIFIED","languageCode": "ja-JP",},"audio": {"uri":"gs://'$bucket'/'$oname'"}}' | jq -r .name)
     sleep 30
     curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" "https://speech.googleapis.com/v1/operations/"$jobid | jq -r ".response.results[] | .alternatives[] | .transcript" > text.txt
