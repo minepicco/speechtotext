39ea84a4e01792bb7c1942e997e88fdf649bbea4
Minehiko Nohara <minepicco@gmail.com>
2019-06-04 19:42:26 +0900

    Update speechtotext.sh

--- speechtotext.sh
+++ speechtotext.sh
@@ -42,15 +42,13 @@ do
         if [ "$cnt" -gt 1 ]; then
             curl -X PATCH -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json" "https://www.googleapis.com/storage/v1/b/"$bucket"/o/"$obj -d '{"metadata": {"text": "Text was exported to '$obj'_.txt"}}'
             n=1$((n+1))
+        elif [ "$line" -eq "null"]
+            echo "The file "$line" is not supported file format. skipping..."
+            n=1$((n+1))
         else
             echo 'The job is not completed sleep another  '$interval' sec until'
             sleep $interval
         fi
     done
-    # curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json; charset=utf-8" "https://speech.googleapis.com/v1/operations/"$line | jq -r ".response.results[] | .alternatives[] | .transcript" > $obj"_.txt"
-    # cnt=`cat $obj"_.txt" | grep -c ""`
-    # if [ "$cnt" -gt 1 ]; then
-    # curl -X PATCH -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) -H "Content-Type: application/json" "https://www.googleapis.com/storage/v1/b/"$bucket"/o/"$obj -d '{"metadata": {"text": "Text was exported to '$obj'_.txt"}}'
-    # fi
     r=$((r+1))
 done < $joblist
